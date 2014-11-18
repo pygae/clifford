e1f045766764fe1d77b6228ab3c423ee3cbec0a1
alex arsenovic <arsenovic@virginia.edu>
2014-11-18 11:27:52 -0500

    made this an installable module

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+VERSION = '0.8'
+LONG_DESCRIPTION = """
+	Geometric algebras (a.k.a. Clifford algebras). 
+"""
+setup(name='clifford',
+	version=VERSION,
+	#license='gpl',
+	description='Geometric algebras (a.k.a. Clifford algebras)',
+	long_description=LONG_DESCRIPTION,
+	author='Robert Kern',
+	author_email='robert.kern@gmail.com',
+	#url='http://scikit-rf.org',
+	packages=find_packages(),
+	install_requires = [
+		'numpy',
+		],
+	package_dir={'clifford':'clifford'},
+	
+	)
+
