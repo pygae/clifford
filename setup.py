2e6d28a02f3edbc02cb533562da03a1fe16133b2
arsenovic <alexanderarsenovic@gmail.com>
2015-08-25 10:22:55 -0400

    removed ez_setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import ez_setup
-ez_setup.use_setuptools()
+#import ez_setup
+#ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
