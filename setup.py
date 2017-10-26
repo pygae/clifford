1bc7865e5f2b2d00c03edf4135e9fdcfff8831e5
arsenovic <alexanderarsenovic@gmail.com>
2017-10-26 09:48:23 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-VERSION = '0.8'
+VERSION = '0.81'
 LONG_DESCRIPTION = """
 A numerical geometric algebra module for python. BSD License.
 """
