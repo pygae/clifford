333c24a414d8eba96ebee0e0bc63ffa4ee5e6cda
Hugo <hadfield.hugo@gmail.com>
2018-07-15 08:19:03 +0100

    Modified some version numbers to allow 0.83 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-VERSION = '0.82'
+VERSION = '0.83'
 LONG_DESCRIPTION = """
 A numerical geometric algebra module for python. BSD License.
 """
