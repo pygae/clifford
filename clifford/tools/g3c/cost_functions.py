67188423c2e29516e09cbd1297eef3e7ecebd837
hugo hadfield <hadfield.hugo@gmail.com>
2018-10-18 16:38:48 +0100

    Updates to improve scene simplification

--- clifford/tools/g3c/cost_functions.py
+++ clifford/tools/g3c/cost_functions.py
@@ -4,7 +4,8 @@ import numpy as np
 from clifford import grade_obj
 from clifford.g3c import *
 import clifford as cf
-from . import rotor_between_objects, rotor_between_lines, val_normalised
+from . import rotor_between_objects, val_rotor_between_lines, \
+    rotor_between_lines, val_normalised, val_rotor_between_objects_root, ConformalMVArray
 
 imt_func = layout.imt_func
 gmt_func = layout.gmt_func
@@ -38,8 +39,10 @@ def rotor_cost(R):
     return val_rotor_cost_sparse(R.value)
 
 
-gradeList = layout.gradeList
+gradeList = np.array(layout.gradeList)
 grade_obj_func = cf.grade_obj_func
+max_float = np.finfo(float).max
+@numba.njit
 def val_object_cost_function(obj_a_val, obj_b_val):
     """
     Evaluates the rotor cost function between two objects
@@ -47,20 +50,17 @@ def val_object_cost_function(obj_a_val, obj_b_val):
     grade_a = grade_obj_func(obj_a_val, gradeList, 0.00001)
     grade_b = grade_obj_func(obj_b_val, gradeList, 0.00001)
     if grade_a != grade_b:
-        return np.finfo(float).max
+        return max_float
     else:
-        R = rotor_between_objects(cf.MultiVector(layout, obj_a_val), cf.MultiVector(layout, obj_b_val))
-        return np.abs(val_rotor_cost_sparse(R.value))
+        R_val = val_rotor_between_objects_root(obj_a_val, obj_b_val)
+        return np.abs(val_rotor_cost_sparse(R_val))
 
 
-def object_cost_function(obj_a,obj_b):
+def object_cost_function(obj_a, obj_b):
     """
     Evaluates the rotor cost function between two objects
     """
-    if grade_obj(obj_a, 0.00001) != grade_obj(obj_b, 0.00001):
-        return np.finfo(float).max
-    R = rotor_between_objects(obj_a.normal(), obj_b.normal())
-    return np.abs(val_rotor_cost_sparse(R.value))
+    return val_object_cost_function(obj_a.value, obj_b.value)
 
 
 def object_set_log_cost_sum(object_set_a, object_set_b, object_type='generic'):
@@ -91,20 +91,45 @@ def object_set_cost_sum(object_set_a, object_set_b, object_type='generic'):
     return sum_val
 
 
+@numba.njit(parallel=True, nogil=True)
+def val_object_set_cost_matrix(object_array_a, object_array_b):
+    """
+    Evaluates the rotor cost matrix between two sets of objects
+    """
+    matrix = np.zeros((object_array_a.shape[0], object_array_b.shape[0]))
+    for a_ind in range(object_array_a.shape[0]):
+        l_a = object_array_a[a_ind, :]
+        for b_ind in range(object_array_b.shape[0]):
+            l_b = object_array_b[b_ind, :]
+            matrix[a_ind, b_ind] = val_object_cost_function(l_a, l_b)
+    return matrix
+
+
+@numba.njit(parallel=True, nogil=True)
+def val_line_set_cost_matrix(object_array_a, object_array_b):
+    """
+    Evaluates the rotor cost matrix between two sets of objects
+    """
+    matrix = np.zeros((object_array_a.shape[0], object_array_b.shape[0]))
+    for a_ind in range(object_array_a.shape[0]):
+        l_a = object_array_a[a_ind, :]
+        for b_ind in range(object_array_b.shape[0]):
+            l_b = object_array_b[b_ind, :]
+            matrix[a_ind, b_ind] = val_line_cost_function(l_a, l_b)
+    return matrix
+
+
 def object_set_cost_matrix(object_set_a, object_set_b, object_type='generic'):
     """
     Evaluates the rotor cost matrix between two sets of objects
     """
-    matrix = np.zeros((len(object_set_a), len(object_set_b)))
+    object_array_a = ConformalMVArray(object_set_a).value
+    object_array_b = ConformalMVArray(object_set_b).value
     if object_type == 'lines':
-        for a_ind, l_a in enumerate(object_set_a):
-            for b_ind, l_b in enumerate(object_set_b):
-                matrix[a_ind, b_ind] = line_cost_function(l_a, l_b)
+        return val_line_set_cost_matrix(object_array_a, object_array_b)
     else:
-        for a_ind, l_a in enumerate(object_set_a):
-            for b_ind, l_b in enumerate(object_set_b):
-                matrix[a_ind, b_ind] = object_cost_function(l_a, l_b)
-    return matrix
+        return val_object_set_cost_matrix(object_array_a, object_array_b)
+
 
 
 def object_set_cost_matrix_sum(object_set_a, object_set_b, object_type='generic'):
@@ -116,11 +141,19 @@ def object_set_cost_matrix_sum(object_set_a, object_set_b, object_type='generic'
     return np.sum(object_set_cost_matrix(object_set_a, object_set_b, object_type))
 
 
-def line_cost_function(obj_a,obj_b):
+@numba.njit
+def val_line_cost_function(obj_a_val, obj_b_val):
+    grade_a = grade_obj_func(obj_a_val, gradeList, 0.00001)
+    grade_b = grade_obj_func(obj_b_val, gradeList, 0.00001)
+    if grade_a != grade_b:
+        return max_float
+    else:
+        R_val = val_rotor_between_lines(obj_a_val, obj_b_val)
+        return np.abs(val_rotor_cost_sparse(R_val))
+
+
+def line_cost_function(obj_a, obj_b):
     """
     Evaluates the rotor cost function between two objects
     """
-    if grade_obj(obj_a, 0.00001) != grade_obj(obj_b, 0.00001):
-        return np.finfo(float).max
-    R = rotor_between_lines(obj_a, obj_b)
-    return np.abs(val_rotor_cost_sparse(R.value))
+    return val_line_cost_function(obj_a.value, obj_b.value)
