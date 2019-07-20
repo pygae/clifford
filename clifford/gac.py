cced09462cf3cc1f9a87b192cdd885a08c02f0c3
hugo hadfield <hadfield.hugo@gmail.com>
2019-07-20 20:23:22 +0100

    Added an implementation of Perwass/BRNO Group GAC

---
+++ clifford/gac.py
@@ -0,0 +1,22 @@
+from . import Cl
+layout, blades = Cl(5, 3)
+locals().update(blades)
+
+n1 = e3 + e6
+n2 = e4 + e7
+n3 = e5 + e8
+
+n1b = 0.5*(e6 - e3)
+n2b = 0.5*(e7 - e4)
+n3b = 0.5*(e8 - e5)
+
+
+def up(x):
+    a = x[e1]
+    b = x[e2]
+    return a*e1 + b*e2 + 0.5*a**2*n1 + n1b + 0.5*b**2*n2 + n2b + a*b*n3
+
+
+def down(x):
+    return (x|e1)[0]*e1 + (x|e2)[0]*e2
+
