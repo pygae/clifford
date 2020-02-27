e332d012d7c56dddb75f651ff32de6ae0f93aacd
Eric Wieser <wieser.eric@gmail.com>
2020-02-27 18:43:18 +0100

    Convert test_speed to a benchmark.
    The benchmark is run in a parallel CI run, to save time.
    
    Also cleans up some clumsy itertools stuff to use the `powerset`	 we use elsewhere.
    This results in the missing tests of scalar and pseudoscalar multiplies being added and fixed.

--- clifford/test/test_algebra_initialisation.py
+++ clifford/test/test_algebra_initialisation.py
@@ -1,22 +1,14 @@
-import itertools
-import time
-
 import numpy as np
 import pytest
 
-from clifford import Cl, conformalize
+from clifford import Cl, conformalize, _powerset
 
 
 class TestInitialisation:
 
-    def test_speed(self):
-        algebras = range(2, 9)
-        print()  # So that the first number is on a new line
-        for i in algebras:
-            t_start = time.time()
-            Cl(i)
-            t_end = time.time()
-            print(i, t_end - t_start)
+    @pytest.mark.parametrize("n", range(2, 9))
+    def test_speed(self, n, benchmark):
+        benchmark(Cl, n)
 
     @pytest.mark.parametrize(
         'algebra',
@@ -28,15 +20,18 @@ class TestInitialisation:
         # Make two random multivectors
         a = layout.randomMV()
         b = layout.randomMV()
-        # Project the multivectors to the grades required
-        grades_possibilities = []
-        for r in range(1, len(layout.sig)):
-            possible_grades = [list(m) for m in list(itertools.combinations(range(len(layout.sig)), r))]
-            grades_possibilities += possible_grades
+
+        # Choose the grades we care about.
+        # We skip the cases of:
+        #  - all grades
+        #  - no grades
+        #  - any multiplications including the pseudoscalar
+        grades_possibilities = list(_powerset(range(layout.dims)))[1:-1]
+
         for i, grades_a in enumerate(grades_possibilities):
-            sparse_mv_a = sum([a(k) for k in grades_a])
+            sparse_mv_a = sum([a(k) for k in grades_a], layout.MultiVector())
             for j, grades_b in enumerate(grades_possibilities):
-                sparse_mv_b = sum([b(k) for k in grades_b])
+                sparse_mv_b = sum([b(k) for k in grades_b], layout.MultiVector())
                 # Compute results
                 gp = layout.gmt_func_generator(grades_a=grades_a, grades_b=grades_b)
                 result_sparse = gp(sparse_mv_a.value, sparse_mv_b.value)
