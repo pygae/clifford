f9ea62dc3368f6ad5475a05989fc6ee76211b4c3
hugo hadfield <hadfield.hugo@gmail.com>
2020-03-09 09:11:44 +0000

    Corrected circle fitting

--- clifford/tools/g3c/object_fitting.py
+++ clifford/tools/g3c/object_fitting.py
@@ -25,14 +25,21 @@ def val_fit_circle(point_list):
     e_vals, e_vecs = np.linalg.eig(accumulator_matrix)
     # Find the smallest and second smallest non negative eigenvalues
     min_eval = np.inf
+    min_eval2 = np.inf
     min_eval_index = -1
     min_eval_index2 = -1
     for i in range(len(e_vals)):
         this_e_val = e_vals[i]
-        if this_e_val < min_eval and this_e_val > 0:
-            min_eval = this_e_val
-            min_eval_index2 = min_eval_index
-            min_eval_index = i
+        if this_e_val > 0:
+            if this_e_val < min_eval:
+                min_eval2 = min_eval
+                min_eval = this_e_val
+                min_eval_index2 = min_eval_index
+                min_eval_index = i
+            else:
+                if this_e_val < min_eval2:
+                    min_eval2 = this_e_val
+                    min_eval_index2 = i
     best_sphere = val_normalised(mask1@np.real(e_vecs[:, min_eval_index]))
     second_best_sphere = val_normalised(mask1@np.real(e_vecs[:, min_eval_index2]))
     best_circle = val_normalised(mask3@dual_func(omt_func(best_sphere, second_best_sphere)))
