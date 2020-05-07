63e2b45dbe95fc99a2e63019e7ed147f1e2b3ca0
hugo hadfield <hadfield.hugo@gmail.com>
2020-05-07 17:27:10 +0100

    Added dpga line tests

--- clifford/test/test_dpga.py
+++ clifford/test/test_dpga.py
@@ -89,7 +89,62 @@ class TestBasicDPGA:
             uporthog = up(vorthog)
             np.testing.assert_allclose((Rt*uporthog*~Rt).value, uporthog.value, atol=1E-4)
 
+            # Points should maintain their distance from the origin
+            pnt_vec = np.random.randn(3)
+            l = np.linalg.norm(pnt_vec)
+            pnt = up(pnt_vec)
+            lres = np.linalg.norm(down(Rt * pnt * ~Rt))
+            np.testing.assert_allclose(l, lres, atol=1E-6)
+
             print()
             print(biv)
             print(vorthog)
             print()
+
+    def test_line(self):
+        for i in range(100):
+            p1vec = np.random.randn(3)
+            p2vec = np.random.randn(3)
+            p1 = up(p1vec)
+            p2 = up(p2vec)
+            line_direc = p2vec - p1vec
+
+            # Plucker line is the outer product of two points or the outer product
+            # of a point and a free vector
+            line = p1 ^ p2
+            free_direc = line_direc[0]*w1 + line_direc[1]*w2 + line_direc[2]*w3
+            line_alt = p1 ^ free_direc
+            assert line_alt == line
+
+            # The line should be the outer product null space
+            lamb = np.random.randn()
+            assert up(lamb*p1vec + (1 - lamb)*p2vec) ^ line == 0
+
+            # Lines can be transformed with rotors
+            tvec = p1vec
+            wt = tvec[0] * w1 + tvec[1] * w2 + tvec[2] * w3
+            Raxis = 1 - w0s * wt
+            assert (~Raxis*line*Raxis) ^ w0 == 0
+
+            # Lines are invariant to screw transformations about their axis
+            axis = p1vec - p2vec
+            rotation_biv = axis[0]*(e23 - e67) + axis[1]*(e57 - e13) + axis[2]*(e12 - e56)
+            Rr = np.e**(-np.random.randn()*rotation_biv)
+            Rt = 1 - w0s * np.random.randn()*(axis[0] * w1 + axis[1] * w2 + axis[2] * w3)
+            np.testing.assert_allclose((Raxis*Rr*Rt*(~Raxis*line*Raxis)*~Rt*~Rr*~Raxis).value,
+                                       line.value, rtol=1E-4, atol=1E-6)
+
+            # A bivector line is invariant under its own exponential
+            Rline = np.e ** line
+            assert Rline*line*~Rline == line
+
+            # The exponential of a line is a rotation about the line
+            line_origin = (~Raxis*line*Raxis)
+            RLineOrigin = np.e ** line_origin
+            random_pnt = up(np.random.randn(3))
+            np.testing.assert_allclose(np.linalg.norm(down(RLineOrigin*random_pnt*~RLineOrigin)),
+                                       np.linalg.norm(down(random_pnt)), rtol=1E-3, atol=1E-4)
+            np.testing.assert_allclose(down(RLineOrigin * (random_pnt + free_direc) * ~RLineOrigin),
+                                       down(RLineOrigin * random_pnt * ~RLineOrigin) + line_direc, rtol=1E-3, atol=1E-4)
+            np.testing.assert_allclose((Rline*~Rline).value, (1 + 0*w1).value, rtol=1E-4, atol=1E-4)
+
