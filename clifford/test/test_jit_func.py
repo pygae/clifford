81601ce0135f8909c54937bd2b32ca4c15bbedc9
hugo hadfield <hadfield.hugo@gmail.com>
2020-06-06 09:17:46 +0100

    Set up pytest benchmark

--- clifford/test/test_jit_func.py
+++ clifford/test/test_jit_func.py
@@ -1,16 +1,20 @@
 import numpy as np
-import time
 from ..jit_func import jit_func
 
 
 class TestJITFunc:
 
-    def test_grade_selection(self):
+    @classmethod
+    def setup_class(cls):
         from clifford.g3c import layout, blades
-        e1 = blades['e1']
-        e12 = blades['e12']
+        cls.layout = layout
+        cls.blades = blades
 
-        @jit_func(layout=layout, ast_debug=True)
+    def test_grade_selection(self):
+        e1 = self.blades['e1']
+        e12 = self.blades['e12']
+
+        @jit_func(layout=self.layout, ast_debug=True)
         def test_func(A, B):
             op = (A+B)(1)
             return op
@@ -21,12 +25,10 @@ class TestJITFunc:
 
         np.testing.assert_allclose(test_func(e1, e12).value, slow_test_func(e1, e12).value)
 
-
     def test_reverse(self):
-        from clifford.g3c import layout, blades
-        e12 = blades['e12']
+        e12 = self.blades['e12']
 
-        @jit_func(layout=layout, ast_debug=True)
+        @jit_func(layout=self.layout, ast_debug=True)
         def test_func(A):
             op = ~A
             return op
@@ -38,13 +40,12 @@ class TestJITFunc:
         np.testing.assert_allclose(test_func(e12).value, slow_test_func(e12).value)
 
     def test_compound_expression(self):
-        from clifford.g3c import layout, blades
-        e1 = blades['e1']
-        e2 = blades['e2']
-        e34 = blades['e34']
-        einf = layout.einf
+        e1 = self.blades['e1']
+        e2 = self.blades['e2']
+        e34 = self.blades['e34']
+        einf = self.layout.einf
 
-        @jit_func(layout=layout, ast_debug=True)
+        @jit_func(layout=self.layout, ast_debug=True)
         def test_func(A, B, C, D):
             op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2)
             return op
@@ -55,36 +56,15 @@ class TestJITFunc:
 
         np.testing.assert_allclose(test_func(e1, e2, einf, e34).value, slow_test_func(e1, e2, einf, e34).value)
 
-    def test_benchmark(self):
-        from clifford.g3c import layout, blades
-        e1 = blades['e1']
-        e2 = blades['e2']
-        e34 = blades['e34']
-        einf = layout.einf
-
-        @jit_func(layout=layout)
+    def test_benchmark_jit(self, benchmark):
+        @jit_func(layout=self.layout)
         def test_func(A, B, C, D):
             op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2)
             return op
+        benchmark(test_func, self.blades['e1'], self.blades['e2'], self.layout.einf, self.blades['e34'])
 
-        def slow_test_func(A, B, C, D):
+    def test_benchmark_nojit(self, benchmark):
+        def test_func(A, B, C, D):
             op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2)
             return op
-
-        print(test_func(e1, e2, einf, e34))
-        print(slow_test_func(e1, e2, einf, e34))
-
-        nrepeats = 10000
-        start_time = time.time()
-        for i in range(nrepeats):
-            test_func(e1, e2, einf, e34)
-        end_time = time.time()
-        print('With jit_func (us): ', 1E6 * (end_time - start_time) / nrepeats)
-
-        nrepeats = 10000
-        start_time = time.time()
-        for i in range(nrepeats):
-            slow_test_func(e1, e2, einf, e34)
-        end_time = time.time()
-        print('Without jit_func (us): ', 1E6 * (end_time - start_time) / nrepeats)
-
+        benchmark(test_func, self.blades['e1'], self.blades['e2'], self.layout.einf, self.blades['e34'])
