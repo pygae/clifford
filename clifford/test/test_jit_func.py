307874f153c775710b6d7a5e34954ca562c77334
hugo hadfield <hadfield.hugo@gmail.com>
2020-06-07 15:12:04 +0100

    Add testing for new jit decorator features

--- clifford/test/test_jit_func.py
+++ clifford/test/test_jit_func.py
@@ -16,7 +16,7 @@ class TestJITFunc:
         e12 = self.blades['e12']
 
         def slow_test_func(A, B):
-            op = (A+B)(1)
+            op = (A(1)+B(2))(1)
             return op
 
         test_func = jit_func(self.layout, ast_debug=True)(slow_test_func)
@@ -38,24 +38,33 @@ class TestJITFunc:
         e1 = self.blades['e1']
         e2 = self.blades['e2']
         e34 = self.blades['e34']
+        e12345 = self.blades['e12345']
         einf = self.layout.einf
 
+        pi = np.pi
         def slow_test_func(A, B, C, D):
-            op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2)
+            op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2) + pi*e12345
             return op
 
-        test_func = jit_func(self.layout, ast_debug=True)(slow_test_func)
+        test_func = jit_func(self.layout,
+                             mv_constants={'e12345': e12345},
+                             scalar_constants={'pi': pi}
+                             )(slow_test_func)
 
         np.testing.assert_allclose(test_func(e1, e2, einf, e34).value, slow_test_func(e1, e2, einf, e34).value)
 
     @pytest.mark.parametrize('use_jit', [False, True])
     def test_benchmark(self, use_jit, benchmark):
-
+        e12345 = self.blades['e12345']
+        pi = np.pi
         def test_func(A, B, C, D):
-            op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2)
+            op = ~(((A * B) * ~C) | (B ^ A)) - 3.1 - A - 7 * B + 5 + C + 2.5 + (2 ^ (A * B * C) ^ 3) + (A | 5) + (A + D)(2) + pi*e12345
             return op
 
         if use_jit:
-            test_func = jit_func(self.layout)(test_func)
+            test_func = jit_func(self.layout,
+                                 mv_constants={'e12345': e12345},
+                                 scalar_constants={'pi': pi}
+                                 )(test_func)
         benchmark(test_func, self.blades['e1'], self.blades['e2'], self.layout.einf, self.blades['e34'])
 
