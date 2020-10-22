e6c4f15066e95ec4ad86f2995a5e50d879a0bee4
hugo hadfield <hadfield.hugo@gmail.com>
2020-10-22 14:32:51 +0100

    Refactor tests for concision

--- clifford/test/test_trig_functions.py
+++ clifford/test/test_trig_functions.py
@@ -1,39 +1,25 @@
 
 from .. import Cl
 import numpy as np
+import cmath
 import pytest
 
 
 class TestScalarProperties:
-
     @pytest.fixture()
     def element(self):
         alg, blades = Cl(0, 0, 0)
         return alg.scalar
 
-    def test_sin(self, element):
+    @pytest.mark.parametrize('np_func', [np.sin,
+                                         np.cos,
+                                         np.tan,
+                                         np.sinh,
+                                         np.cosh,
+                                         np.tanh])
+    def test_trig(self, element, np_func):
         for x in np.linspace(0, 2*np.pi, 100):
-            assert abs(np.sin(x*element).value[0] - np.sin(x)) < 1E-10
-
-    def test_cos(self, element):
-        for x in np.linspace(0, 2*np.pi, 100):
-            assert abs(np.cos(x*element).value[0] - np.cos(x)) < 1E-10
-
-    def test_tan(self, element):
-        for x in np.linspace(-0.9*np.pi/2, 0.9*np.pi/2, 100):
-            assert abs(np.tan(x*element).value[0] - np.tan(x)) < 1E-10
-
-    def test_sinh(self, element):
-        for x in np.linspace(0, 2*np.pi, 100):
-            assert abs(np.sinh(x*element).value[0] - np.sinh(x)) < 1E-10
-
-    def test_cosh(self, element):
-        for x in np.linspace(0, 2*np.pi, 100):
-            assert abs(np.cosh(x*element).value[0] - np.cosh(x)) < 1E-10
-
-    def test_tanh(self, element):
-        for x in np.linspace(-np.pi, np.pi, 100):
-            assert abs(np.tanh(x*element).value[0] - np.tanh(x)) < 1E-10
+            assert abs(np_func(x*element).value[0] - np_func(x)) < 1E-10
 
 
 class TestDualNumberProperties:
@@ -42,38 +28,35 @@ class TestDualNumberProperties:
         alg, blades = Cl(0, 0, 1)
         return alg.scalar, blades['e1']
 
-    def test_sin(self, element):
+    @pytest.mark.parametrize('func, deriv_func', [(np.sin, np.cos),
+                                                  (np.cos, lambda x: -np.sin(x)),
+                                                  (np.tan, lambda x: (1/np.cos(x)**2)),
+                                                  (np.sinh, np.cosh),
+                                                  (np.cosh, np.sinh),
+                                                  (np.tanh, lambda x: (1 - np.tanh(x)**2))])
+    def test_derivatives(self, element, func, deriv_func):
         for x in np.linspace(0, 2*np.pi, 10):
-            result = np.sin(x * element[0] + element[1])
-            assert abs(result.value[0] - np.sin(x)) < 1E-10
-            assert abs(result.value[1] - np.cos(x)) < 1E-10
+            result = func(x * element[0] + element[1])
+            assert abs(result.value[0] - func(x)) < 1E-10
+            assert abs(result.value[1] - deriv_func(x)) < 1E-10
 
-    def test_cos(self, element):
-        for x in np.linspace(0, 2*np.pi, 10):
-            result = np.cos(x * element[0] + element[1])
-            assert abs(result.value[0] - np.cos(x)) < 1E-10
-            assert abs(result.value[1] + np.sin(x)) < 1E-10
 
-    def test_tan(self, element):
-        for x in np.linspace(0, 2*np.pi, 10):
-            result = np.tan(x * element[0] + element[1])
-            assert abs(result.value[0] - np.tan(x)) < 1E-10
-            assert abs(result.value[1] - (1/np.cos(x)**2)) < 1E-10
+class TestComplexNumberProperties:
+    @pytest.fixture()
+    def element(self):
+        alg, blades = Cl(0, 1, 0)
+        return alg.scalar, blades['e1']
 
-    def test_sinh(self, element):
-        for x in np.linspace(0, 2*np.pi, 10):
-            result = np.sinh(x * element[0] + element[1])
-            assert abs(result.value[0] - np.sinh(x)) < 1E-10
-            assert abs(result.value[1] - np.cosh(x)) < 1E-10
-
-    def test_cosh(self, element):
-        for x in np.linspace(0, 2*np.pi, 10):
-            result = np.cosh(x * element[0] + element[1])
-            assert abs(result.value[0] - np.cosh(x)) < 1E-10
-            assert abs(result.value[1] - np.sinh(x)) < 1E-10
-
-    def test_tanh(self, element):
-        for x in np.linspace(0, 2*np.pi, 10):
-            result = np.tanh(x * element[0] + element[1])
-            assert abs(result.value[0] - np.tanh(x)) < 1E-10
-            assert abs(result.value[1] - (1 - np.tanh(x)**2)) < 1E-10
+    @pytest.mark.parametrize('np_func,cmath_func', [(np.sin, cmath.sin),
+                                                    (np.cos, cmath.cos),
+                                                    (np.tan, cmath.tan),
+                                                    (np.sinh, cmath.sinh),
+                                                    (np.cosh, cmath.cosh),
+                                                    (np.tanh, cmath.tanh)])
+    def test_trig(self, element, np_func, cmath_func):
+        for x in np.linspace(0, 2 * np.pi, 10):
+            for y in np.linspace(0, 2 * np.pi, 10):
+                complex_mv = x * element[0] + y * element[1]
+                complex_value = x + 1j * y
+                result = np_func(complex_mv)
+                assert abs(result.value[0] + 1j * result.value[1] - cmath_func(complex_value)) < 1E-10
