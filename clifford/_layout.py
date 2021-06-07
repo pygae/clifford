f4e56dafd2319bed44d4acb862ead62ac4449697
tbuli <tbuli@Martins-MacBook-Pro.local>
2021-06-07 11:44:44 +0200

    Stop checking if a MV matrix is invertible and let numpy decide.

--- clifford/_layout.py
+++ clifford/_layout.py
@@ -671,8 +671,6 @@ class Layout(object):
         @_numba_utils.njit
         def leftLaInvJIT(value):
             intermed = _numba_val_get_left_gmt_matrix(value, k_list, l_list, m_list, mult_table_vals, n_dims)
-            if abs(np.linalg.det(intermed)) < _settings._eps:
-                raise ValueError("multivector has no left-inverse")
             sol = np.linalg.solve(intermed, identity)
             return sol
 
