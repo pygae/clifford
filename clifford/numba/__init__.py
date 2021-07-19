25fb75cb945e7d46f4b0909e73d4457d5f474e4c
Eric Wieser <wieser.eric@gmail.com>
2021-07-19 13:11:41 +0100

    Update the release notes

--- clifford/numba/__init__.py
+++ clifford/numba/__init__.py
@@ -1,2 +1,93 @@
+"""
+.. currentmodule:: clifford.numba
+
+===============================================
+numba extension support (:mod:`clifford.numba`)
+===============================================
+
+This module provides :mod:`numba` extension types :class:`MultiVectorType` and
+:class:`LayoutType` corresponding to :class:`~clifford.MultiVector` and
+:class:`~clifford.Layout`.
+
+You do not need to import this module to take advantage of these types; they
+are needed only directly when writing numba overloads via
+:func:`numba.extending.overload` and similar.
+
+.. currentmodule:: clifford
+
+Supported operations
+--------------------
+The following list of operations are supported in a jitted context:
+
+* A limited version of the constructor ``MultiVector(layout, value)``, and
+  the alias :meth:`layout.MultiVector`.
+* :meth:`MultiVector.value`
+* :meth:`MultiVector.layout`
+* Arithmetic:
+  * :meth:`MultiVector.__add__`
+  * :meth:`MultiVector.__sub__`
+  * :meth:`MultiVector.__mul__`
+  * :meth:`MultiVector.__xor__`
+  * :meth:`MultiVector.__or__`
+  * :meth:`MultiVector.__pow__`
+  * :meth:`MultiVector.__truediv__`
+  * :meth:`MultiVector.__invert__`
+  * :meth:`MultiVector.__pos__`
+  * :meth:`MultiVector.__neg__`
+* :meth:`MultiVector.__call__`
+* :meth:`MultiVector.mag2`
+* :meth:`MultiVector.__abs__`
+* :meth:`MultiVector.normal`
+* :meth:`MultiVector.gradeInvol`
+* :meth:`MultiVector.conjugate`
+
+Performance considerations
+--------------------------
+While the resulted jitted code is typically faster, there are two main
+performance issues to consider. The first is the startup time of ``@jit``ing.
+This can be quite substantial, although can be somewhat alleviated by using
+the ``cache=True`` argument to :func:`numba.njit`
+The second is the time taken for numba to find the appropriate dispatch loop
+given the Python types of its arguments, which adds overhead to every call.
+
+``clifford`` tries as hard as possible to reduce this second overhead, by
+using the undocumented ``_numba_type_`` attribute and keeping our own optimized
+cache instead of going through the recommended
+``@numba.extending.typeof_impl.register(LayoutType)`` mechanism.
+However, this overhead can still be slow compared to elementary operations.
+The following code is significantly impacted by this overhead::
+
+    from clifford.g3c import *
+    import numba
+
+    @numba.njit
+    def mul(a, b):
+        return a * b
+
+    # 286 ms, ignoring jitting time
+    x = e1
+    for i in range(10000):
+        x = mul(x, x + e2)
+
+as each iteration of the loop pays it again. The overhead can be avoided by
+jitting the entire loop::
+
+    from clifford.g3c import *
+    import numba
+
+    @numba.njit
+    def mul(a, b):
+        return a * b
+
+    @numba.njit
+    def the_loop(x):
+        for i in range(1000):
+            x = mul(x, x + e1)
+        return x
+
+    # 2.4 ms, ignoring jitting time
+    x = the_loop(eq)
+
+"""
 from ._multivector import MultiVectorType
 from ._layout import LayoutType
--- docs/changelog.rst
+++ docs/changelog.rst
@@ -6,6 +6,18 @@ Changelog
 Changes in 1.4.x
 ++++++++++++++++
 
+ * :class:`MultiVector` is now supported directly within ``@numba.njit``\ ed code. As an example:
+   ```
+   from clifford.g3c import *
+
+   @numba.njit
+   def jit_up(x):
+       return eo + x + 0.5*abs(x)**2*einf
+
+   assert up(e1) == jit_up
+   ```
+   See :mod:`clifford.numba` for details.
+
  * Projection using :meth:`Multivector.__call__` no longer raises :exc:`ValueError`
    for grades not present in the algebra, and instead just returns zero.
 
@@ -18,10 +30,26 @@ Changes in 1.4.x
 
  * A new :mod:`clifford.taylor_expansions` is added to implement taylor series of various
    multivector functions, starting with common trigonometric functions. These functions are
-   additionally exposed via methods on the MultiVector class itself.
+   additionally exposed via methods on :class:`MultiVector` like :meth:`Multivector.cos`.
 
  * Random functions now accept an ``rng`` keyword argument that accepts the object returned
    by :func:`numpy.random.default_rng`, for deterministic randomness.
+
+Bugs fixed
+----------
+* ``mv(grade)`` (aka :meth:`Multivector.__call__`) now returns ``0`` instead of throwing an
+  exception if ``grade`` is out of range.
+
+
+Compatibility notes
+-------------------
+* :func:`clifford.general_exp` is deprecated in favor of :meth:`clifford.taylor_expansions.exp`,
+  although typically :meth:`clifford.MultiVector.exp` is a better choice
+* Transparently treating a :class:`MultiVector` as a flat array of coefficients is deprecated,
+  and so ``mv[0]`` and ``len(mv)`` both emit :exc:`DeprecationWarning`s. If the underlying
+  storage order is of interest, use ``mv.value[0]`` and ``len(mv)``respectively. To obtain the
+  scalar part of a :class:`MultiVector`, use ``mv[()]`` instead of ``mv[0]``.
+
 
 Changes in 1.3.x
 ++++++++++++++++
