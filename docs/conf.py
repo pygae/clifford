04fd2a330a52c242fbbf201bbd1c55cf4b3675e1
Eric Wieser <wieser.eric@gmail.com>
2021-07-23 10:12:54 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,8 @@ nbsphinx_prolog = r"""
 .. raw:: html
 
     <div class="admonition note">
-      <pre>}{{ config }}</pre>
-      <pre>}{{ config.html_context }}</pre>
+      <pre>}{{ env.config }}</pre>
+      <pre>}{{ env.config.html_context }}</pre>
       <p>This page was generated from
         <a class="reference external" href="https://github.com/pygae/clifford/blob/{{ git_ref|e }}/{{ docname|e }}">{{ docname|e }}</a>.
         Interactive online version:
